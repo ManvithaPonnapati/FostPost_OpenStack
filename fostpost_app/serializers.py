dc6ba9ff018239a7207eddd8fad5af39ba6e89d5
ManvithaPonnapati <rp493@cornell.edu>
2016-06-08 16:32:11 -0400

    Serializers added for user alone.

--- fostpost_app/serializers.py
+++ fostpost_app/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    email = serializers.CharField(required=True, allow_blank=False, max_length=100)
+    
+    def create(self, validated_data):
+        """
+        Create and return a new `User` instance, given the validated data.
+        """
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `User` instance, given the validated data.
+        """
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
