4993db8dce20b06597350e990df5d01505af574d
ManvithaPonnapati <rp493@cornell.edu>
2016-06-10 10:22:32 -0400

    Updated and final models. Run Db_migrator to update your postgres connections - python manage.py migrate

--- fostpost_app/models.py
+++ fostpost_app/models.py
@@ -1,29 +1,63 @@
-from __future__ import unicode_literals
+__author__ = 'manvitha_ponnapati'
 
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
 class User(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    email = models.CharField(max_length=100, blank=False, default='')
-    
-class Image_Asset(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=100, blank=False, default='')
- 
-class Logo_Asset(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=100, blank=False, default='')   
+    """ An awesome fostpost customer"""
+    email = models.CharField(max_length=100)
+    created_date = models.CharField(max_length=500)
+   
 
-class Saved_Creative(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=100, blank=False, default='')
-    background_image=models.ImageField()
-    logo_image=models.ImageField()
-    string_array=models.ArrayField()
-    font_size_array=models.ArrayField()
-    text_color=models.CharField(max_length=15)
-    background_color=models.CharField(max_length=15)
-    positions_text=models.ArrayField()
-    positions_images=models.ArrayField()
-    position_logo=models.ArrayField()
+
+class asset_image(models.Model):
+    """ Model for any uploaded images to the database."""
+    user = models.ForeignKey(User)
+    image_upload = models.ImageField(upload_to=image_file_name)
+
+class asset_logo(models.Model):
+    """ Model for any uploaded logos to the database."""
+    user = models.ForeignKey(User)
+    image_upload = models.ImageField(upload_to=image_file_name)
+
+
+class saved_creative(models.Model):
+    """ Model for any saved creatives to the database."""
+    user = models.ForeignKey(User)
+    image_array= models.CharField(max_length=200)
+    logo=models.CharField(max_length=200)
+    text_array=models.CharField(max_length=200)
+    text_positions=models.CharField(max_length=200)
+    creative_size_x=models.IntegerField(max_length=10)
+    creative_size_y=models.IntegerField(max_length=10)
+    def set_image_array(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_image_array(self, x):
+        return json.loads(self.image_array)
+    def set_logo(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_logo(self, x):
+        return json.loads(self.image_array)
+
+    def set_text_strings(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_text_strings(self, x):
+        return json.loads(self.image_array)
+
+    def set_text_positions(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_text_postions(self, x):
+        return json.loads(self.image_array)
+
+
+def image_file_name(instance):
+    return '/'.join(['image_asset', instance.user.email, "%Y %M %d:%H:%M:%S"])
+
+def logo_file_name(instance):
+    return '/'.join(['logo_asset', instance.user.email, "%Y %M %d:%H:%M:%S"])
--- fostpost_app/serializers.py
+++ fostpost_app/serializers.py
@@ -1,19 +1,61 @@
-from rest_framework import serializers
+from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class UserSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    email = serializers.CharField(required=True, allow_blank=False, max_length=100)
+
+class User(models.Model):
+    """ An awesome fostpost customer"""
+    email = models.CharField(max_length=100)
+    created_date = models.CharField(max_length=500)
+   
+
+
+class asset_image(models.Model):
+    """ Model for any uploaded images to the database."""
+    user = models.ForeignKey(User)
+    image_upload = models.ImageField(upload_to=image_file_name)
+
+class asset_logo(models.Model):
+    """ Model for any uploaded images to the database."""
+    user = models.ForeignKey(User)
+    image_upload = models.ImageField(upload_to=image_file_name)
+
+
+class saved_creative(models.Model):
     
-    def create(self, validated_data):
-        """
-        Create and return a new `User` instance, given the validated data.
-        """
-        return User.objects.create(**validated_data)
+    user = models.ForeignKey(User)
+    image_array= models.CharField(max_length=200)
+    logo=models.CharField(max_length=200)
+    text_array=models.CharField(max_length=200)
+    text_positions=models.CharField(max_length=200)
+    creative_size_x=models.IntegerField(max_length=10)
+    creative_size_y=models.IntegerField(max_length=10)
+    def set_image_array(self, x):
+        self.image_array = json.dumps(x)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `User` instance, given the validated data.
-        """
-        instance.email = validated_data.get('email', instance.email)
-        instance.save()
-        return instance
+    def get_image_array(self, x):
+        return json.loads(self.image_array)
+    def set_logo(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_logo(self, x):
+        return json.loads(self.image_array)
+
+    def set_text_strings(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_text_strings(self, x):
+        return json.loads(self.image_array)
+
+    def set_text_positions(self, x):
+        self.image_array = json.dumps(x)
+
+    def get_text_postions(self, x):
+        return json.loads(self.image_array)
+
+
+def image_file_name(instance):
+    return '/'.join(['image_asset', instance.user.email, "%Y %M %d:%H:%M:%S"])
+
+def logo_file_name(instance):
+    return '/'.join(['logo_asset', instance.user.email, "%Y %M %d:%H:%M:%S"])
