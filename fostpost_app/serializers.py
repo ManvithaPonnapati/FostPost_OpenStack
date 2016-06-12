42a74fbfc64c85f5d7f710bee6c13d3262d84448
ManvithaPonnapati <rp493@cornell.edu>
2016-06-12 18:57:37 -0400

    Finished serializers for DJango REST and models. At this point we have all the parts of stack connected for user accounts

---
+++ fostpost_app/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import update_session_auth_hash
+
+from rest_framework import serializers
+
+from fostpost_app.models import CraftCloud_Account
+
+
+class CraftCloud_AccountSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    confirm_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = CraftCloud_Account
+        fields = ('id', 'email', 'username', 'created_at','password',
+                  'confirm_password',)
+        read_only_fields = ('created_at')
+
+        def create(self, validated_data):
+            return CraftCloud_Account.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            instance.username = validated_data.get('username', instance.username)
+            instance.save()
+
+            password = validated_data.get('password', None)
+            confirm_password = validated_data.get('confirm_password', None)
+
+            if password and confirm_password and password == confirm_password:
+                instance.set_password(password)
+                instance.save()
+
+            update_session_auth_hash(self.context.get('request'), instance)
+
+            return instance
