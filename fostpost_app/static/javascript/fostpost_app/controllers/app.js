92e74a81830d91708f801520d9aa57d81f1edce9
ManvithaPonnapati <rp493@cornell.edu>
2016-06-30 15:46:32 -0400

    Attaching ids to nav bar items

--- fostpost_app/static/javascript/fostpost_app/controllers/app.js
+++ fostpost_app/static/javascript/fostpost_app/controllers/app.js
@@ -6,6 +6,13 @@ var app = angular.module('angularjs-starter', []);
 
 app.controller('createCtrl', function($scope) {
   console.log("Inside create");
+  //1-Images
+  //2-Layouts
+  //3-fonts
+  //4-Logo
+  //5-Save
+  //6-Colors
+  $scope.selected_navbar_item=1  //Images nav bar item is selected by default
   $scope.facebookSizes={"Conversions": "1200x628", "Post Page Engagement": "1200x900", "Carousel": "600x600"};
   console.log($scope.facebookSizes)
   $scope.googleSizes={"Large Rectangle": "336x280", "Medium Rectangle": "300x250", "Leaderboard": "728x90","Half Page":"300x600","Large Mobile Banner":"320x100"};
