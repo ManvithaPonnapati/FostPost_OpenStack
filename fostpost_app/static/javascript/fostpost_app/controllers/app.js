6ebc8eecaee8bd49cf9926c6eae1eb7248e976e3
ManvithaPonnapati <rp493@cornell.edu>
2016-07-19 07:52:52 -0400

    adding more canvas code

--- fostpost_app/static/javascript/fostpost_app/controllers/app.js
+++ fostpost_app/static/javascript/fostpost_app/controllers/app.js
@@ -21,6 +21,8 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
   $scope.image_sizesH=[628];
   $scope.logo_sizeW=10;
   $scope.logo_sizeH=10;
+  $scope.logo_X=0
+  $scope.logo_Y=100
   $scope.background_r=230;
   $scope.background_g=221;
   $scope.background_b=236;
@@ -29,8 +31,8 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
   $scope.text_x=[20,20,20]
   $scope.text_y=[100,140,180]
   $scope.text_font=["Arial"]
-  $scope.text_fontSize=["32","16","8"]
-  $scope.text_w=[]
+  $scope.text_fontSize=[32,16,16]
+  $scope.text_w=[0,0,0]
   $scope.text_padding=5
   $scope.selected_item=1
   $scope.html_test=$sce.trustAsHtml("<img  src='{% static 'svg/layout.svg' %}' style='width: 50%;height:65%' align='middle' />")
@@ -163,13 +165,67 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
     var c=document.getElementById('adcanvas');
     var processingInstance1=new Processing(c,sketchProc);
     function sketchProc(processing) {
-      var background_image=new processing.PImage;
+            var background_image=new processing.PImage;
+            var online1=new processing.PImage;
+            var delButton=new processing.PImage;
+            var logoResize=new processing.PImage;
+            var sizearrow=new processing.PImage;
+            processing.size(parseInt($scope.canvas_width),parseInt($scope.canvas_height))
+            $('#adcanvas').css('width',parseInt($scope.canvas_width));
+            $('#adcanvas').css('height',parseInt($scope.canvas_height));
+              adcanvas.width=parseInt($scope.canvas_width);
+              adcanvas.height=parseInt($scope.canvas_height);
+
+
+              
+            var imx=parseInt($scope.image_positionsX[0]);
+            var imy=parseInt($scope.image_positionsY[0]);
+            
+            var imw=parseInt($scope.image_sizesW[0]);
+            var imh=parseInt($scope.image_sizesH[0]);
+            ////console.log(imw,imh)
+            var extrax=imx
+            var extray=-imh/2
+            var im1x=parseInt($scope.logo_X);
+            var im1y=parseInt($scope.logo_Y);
+            var im1w=$scope.logo_sizeW;
+          
+            ////console.log(im1w,imw,im1x)
+            var im1h=$scope.logo_sizeH;
+            var cwprev=$scope.canvas_width;
+            var chprev=$scope.canvas_height;
+            var ratio=1;
+            
+            
+            var dragimx=0;
+            var dragimy=0;
+            var dragim1x=0;
+            var dragim1y=0;
+            var dragged=0;
+            
+            //var rx=parseInt(sessionStorage.textx);
+            //var ry=parseInt(sessionStorage.texty);
+            ////console.log(rx,ry)
+            
+            
+            //var fontfamily=getfontfamily()
+            //sessionStorage.ffamily=fontfamily
+            ////console.log(fontfamily)
+            //fontfamily=processing.createFont(sessionStorage.ffamily)
+            var rect_select=10;
+            
+            //var myFont=processing.createFont("ContrailOne-Regular");
+            var exta=new processing.PImage;
+      ////console.log($scope.image_sizesH[0])
       processing.setup=function()
       {
 
         processing.size($scope.canvas_width,$scope.canvas_height)
         processing.background($scope.background_r,$scope.background_g,$scope.background_b)
         background_image=processing.requestImage($scope.image_sources[0])
+        delButton=processing.requestImage(DJANGO_STATIC_URL+"images/deletetextbox.png")
+        logoResize=processing.requestImage(DJANGO_STATIC_URL+"svg/logosize.svg")
+        sizearrow=processing.requestImage(DJANGO_STATIC_URL+"images/sizearrow.png")
         console.log(background_image)
 
 
@@ -188,7 +244,6 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
           processing.noFill()
           processing.stroke(0)
           processing.rect($scope.text_x[i],$scope.text_y[i]-$scope.text_fontSize[i],$scope.text_x[i]+$scope.text_w[i],$scope.text_fontSize[i])
-          
         }
 
       }
@@ -196,8 +251,17 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
       {
         var mx=processing.mouseX;
         var my=processing.mouseY;
-        
-        if(processing.mouseX>$scope.image_positionsX[0] && processing.mouseX < ($scope.image_positionsX[0]+$scope.image_sizesW[0]) && processing.mouseY > $scope.image_positionsY[0] && processing.mouseY<($scope.image_positionsY[0]+$scope.image_sizesH[0]))
+        for(i=0;i<$scope.text_array.length;i++)
+        {
+          console.log($scope.text_x,$scope.text_y,$scope.text_w,$scope.text_fontSize)
+          if( processing.mouseX>$scope.text_x[i] && processing.mouseX>$scope.text_x[i]+$scope.text_w[i] && processing.mouseY>$scope.text_y[i]-$scope.text_fontSize[i] && processing.mouseY > ($scope.text_y[i]))
+          {
+            text_mouseFlags[0]=1
+            console.log("XXXX")
+            console.log(i)
+          }
+        }
+        if($scope.text_mouseFlags==[0,0,0] && processing.mouseX>$scope.image_positionsX[0] && processing.mouseX < ($scope.image_positionsX[0]+$scope.image_sizesW[0]) && processing.mouseY > $scope.image_positionsY[0] && processing.mouseY<($scope.image_positionsY[0]+$scope.image_sizesH[0]))
         {
           $scope.image_mouseFlag=1
           images_dragX[0]=$scope.image_positionsX[0]-mx
