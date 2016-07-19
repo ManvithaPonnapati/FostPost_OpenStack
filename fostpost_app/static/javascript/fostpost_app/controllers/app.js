d18193eda2e2552f86c009770fed6fd562aed5c1
ManvithaPonnapati <rp493@cornell.edu>
2016-07-18 22:40:43 -0400

    adding wrap widths to text

--- fostpost_app/static/javascript/fostpost_app/controllers/app.js
+++ fostpost_app/static/javascript/fostpost_app/controllers/app.js
@@ -25,12 +25,13 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
   $scope.background_g=221;
   $scope.background_b=236;
   $scope.background_img="{% static 'images/placeholdergoog_360.jpg' %}"
-  $scope.text_array=[]
+  $scope.text_array=['','','']
   $scope.text_x=[20,20,20]
   $scope.text_y=[100,140,180]
   $scope.text_font=["Arial"]
   $scope.text_fontSize=["32","16","8"]
   $scope.text_w=[]
+  $scope.text_padding=5
   $scope.selected_item=1
   $scope.html_test=$sce.trustAsHtml("<img  src='{% static 'svg/layout.svg' %}' style='width: 50%;height:65%' align='middle' />")
   $scope.parentWidth=(document.getElementById("canvasParent")).clientWidth;
@@ -178,11 +179,16 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
         $scope.text_array=[$scope.MainText,$scope.SmallText,$scope.BodyText]
         processing.background($scope.background_r,$scope.background_g,$scope.background_b)
         processing.image(background_image,$scope.image_positionsX[0],$scope.image_positionsY[0],$scope.image_sizesW[0],$scope.image_sizesH[0])
+
         for(i=0;i<$scope.text_array.length;i++)
         {
           processing.textFont("Arial",$scope.text_fontSize[i])
           $scope.text_w[i]=processing.textWidth($scope.text_array[i])
           processing.text($scope.text_array[i],$scope.text_x[i],$scope.text_y[i])
+          processing.noFill()
+          processing.stroke(0)
+          processing.rect($scope.text_x[i],$scope.text_y[i]-$scope.text_fontSize[i],$scope.text_x[i]+$scope.text_w[i],$scope.text_fontSize[i])
+          
         }
 
       }
@@ -197,32 +203,18 @@ app.controller('createCtrl', function($scope,$http,$sce,$window) {
           images_dragX[0]=$scope.image_positionsX[0]-mx
           images_dragY[0]=$scope.image_positionsY[0]-my
         }
-        for(i=0;i<$scope.text_array.length;i++)
-        {
-          if(processing.mouseX>$scope.text_x[i] && processing.mouseX < ($scope.text_x[i]+$scope.text_w[i]) && processing.mouseY > $scope.text_y[i] && processing.mouseY<($scope.text_y[i]+$scope.text_fontSize[i]))
-          {
-              $scope.text_mouseFlags=[0,0,0]
-              $scope.text_mouseFlags[i]=1
-          }
-        }
+        
 
       }
       processing.mouseDragged=function()
       {
           if($scope.image_mouseFlag==1)
           {
+            dragging=1
             $scope.image_positionsX[0]=processing.mouseX+images_dragX[0]
             $scope.image_positionsY[0]=processing.mouseY+images_dragY[0]
           }
-          for(i=0;i<$scope.text_array.length;i++)
-          {
-            if($scope.text_mouseFlags[i]==1)
-            {
-                $scope.image_mouseFlag=0
-                $scope.text_x[i]=processing.mouseX
-                $scope.text_y[i]=processing.mouseY
-            }
-          }
+         
       }
       processing.mouseReleased=function()
       {  
