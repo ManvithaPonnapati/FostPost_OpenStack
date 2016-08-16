9ec5bf82869a3bb457ea76462d717399c70b4772
ManvithaPonnapati <rp493@cornell.edu>
2016-08-16 00:06:06 -0400

    changing images

--- fostpost_app/static/javascript/fostpost_app/controllers/app.js
+++ fostpost_app/static/javascript/fostpost_app/controllers/app.js
@@ -152,7 +152,7 @@ app.controller('createCtrl', function($scope,$http,$sce,$window,Upload) {
   $scope.MainText="Drag your own image onto the canvas"
   $scope.SmallText=" "
   $scope.BodyText=" "
-  $scope.font_imageArray=["/static/svg/font1.svg","/static/svg/font2.svg","/static/svg/font3.svg","/static/svg/font4.svg","/static/svg/font5.svg","/static/svg/font6.svg","/static/svg/font7.svg"]
+  $scope.font_imageArray=["/static/svg/font1.svg","/static/svg/font2.svg","/static/svg/font3.svg","/static/svg/font4.svg","/static/svg/font5.svg","/static/svg/font6.svg"]
   $scope.canvas_color='#dfd333'
   var copywidth=$scope.canvas_width;
   $scope.selected_navbar_item=1  //Layouts nav bar item is selected by default
