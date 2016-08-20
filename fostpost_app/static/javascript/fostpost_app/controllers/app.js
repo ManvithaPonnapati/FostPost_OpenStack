49954cfe72a09357df7f0085dc36983a245abf43
ManvithaPonnapati <rp493@cornell.edu>
2016-08-19 22:17:00 -0400

    attaching color picker to cnavas

--- fostpost_app/static/javascript/fostpost_app/controllers/app.js
+++ fostpost_app/static/javascript/fostpost_app/controllers/app.js
@@ -26,7 +26,14 @@ app.directive('scrolly', function () {
 
 app.controller('createCtrl', function($scope,$http,$sce,$window,Upload) {
   console.log("Inside create");
-  
+  $scope.scopeVariable={}
+  $scope.scopeVariable.options = {
+    label: "Choose a color",
+    icon: "brush",
+    default: "#f00",
+    genericPalette: false,
+    history: false
+};
   $scope.unsplash = 0
   $scope.craftcloud_item = function ()
   {
