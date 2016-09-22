9e0155519a316c2b809f369184ca436b8512c63c
ManvithaPonnapati <rp493@cornell.edu>
2016-09-21 20:13:55 -0400

    e

--- FostPost/settings.py
+++ FostPost/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yc1%*2%6or8_0x88zmvz04lcp))02ziay8473i0eb8%_d#j9w0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
